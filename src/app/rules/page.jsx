56767fcc4cdda01f3f1a6133d61cf35e5c84dac0
Zaldy Thalib <zaldy222@gmail.com>
2024-11-03 23:39:13 +1100

    Added temporary pages in /rules and /book

---
+++ src/app/rules/page.jsx
@@ -0,0 +1,7 @@
+export default function AdminHome() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Coming Soon!</h1>
+    </main>
+  );
+}
